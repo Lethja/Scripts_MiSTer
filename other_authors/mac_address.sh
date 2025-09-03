1b68d10b7e55f7e5f78e50b6294005bd5cdc1348
Jason Lethbridge <lethbridgejason@gmail.com>
2025-09-03 12:04:35 +1000

    Initial implementation of mac_address.sh

---
+++ other_authors/mac_address.sh
@@ -0,0 +1,151 @@
+iface="${1:-eth0}"
+
+scan() {
+	local cidr=$(ip -o -f inet addr show $iface | awk 'NR==0 {exit 1} {print $4}')
+	if [ -n "$cidr" ]; then
+		local msg="Discovering MAC addresses on $iface network. Please wait..."
+		local len=$(( ${#msg} + 4 ))
+		len=$(( len + (len % 2) ))
+
+		dialog --infobox "$msg" 3 "$len" 2>&1 >/dev/tty
+		fping -q -r 1 -t 100 -I $iface -g $cidr
+		ip neigh | awk '{print $5}' | sort -u
+		return
+	else
+		local msg="There doesn't appear to be a network connection on $iface. Would you like to continue anyway?"
+		local len=$(( ${#msg} + 4 ))
+		len=$(( len + (len % 2) ))
+
+		dialog --yesno "$msg" 6 "$len" 2>&1 >/dev/tty
+		if [ $? -eq 0 ]; then
+			echo ""
+			return
+		fi
+	fi
+}
+
+gen() {
+	local msg="Generating MAC address for $iface. Please wait..."
+	local len=$(( ${#msg} + 4 ))
+	len=$(( len + (len % 2) ))
+
+	dialog --infobox "$msg" 3 "$len" 2>&1 >/dev/tty
+	local blacklist
+	mapfile -t blacklist <<< "$1"
+	while :; do
+		local hex=$(tr -dc '0-9a-f' </dev/urandom | head -c 12)
+		local mac=$(echo $hex | sed 's/../&:/g; s/:$//')
+
+		# Skip special addresses
+		first_byte=$((16#${hex:0:2}))
+		if (( first_byte & 1 )) || [[ ${mac:0:2} == "02" ]] || [[ "$mac" == "FF:FF:FF:FF:FF:FF" ]] || [[ "$mac" == "00:00:00:00:00:00" ]]; then
+			continue
+		fi
+
+		# Skip address already in use
+		local skip=0
+		for b in "${blacklist[@]}"; do
+			[[ "$mac" == "$b" ]] && skip=1 && break
+		done
+		[[ $skip -eq 1 ]] && continue
+
+		echo "$mac"
+		break
+	done
+}
+
+update_uboot() {
+	local file="/media/fat/linux/u-boot.txt"
+	if [ -n "$1" ] && [ "$iface" = "eth0" ]; then
+		if [ ! -f "$file" ]; then
+			echo "ethaddr=${1^^}" > "$file"
+		elif grep -q '^ethaddr=' "$file"; then
+			sed -i "s/^ethaddr=.*/ethaddr=${1^^}/" "$file"
+		else
+			echo "ethaddr=${1^^}" >> "$file"
+		fi
+	fi
+}
+
+update_mac() {
+	local msg="Configuring MAC address for $iface. Please wait..."
+	local len=$(( ${#msg} + 4 ))
+	len=$(( len + (len % 2) ))
+
+	dialog --infobox "$msg" 3 "$len" 2>&1 >/dev/tty
+	dhcpcd -k 2>/dev/null 1>/dev/null # Sadly can't specify a iface here due to dhcpcd bug
+	ip link set dev "$iface" down 2>/dev/null 1>/dev/null
+	ip link set dev "$iface" address "$mac" 2>/dev/null 1>/dev/null
+	ip link set dev "$iface" up 2>/dev/null 1>/dev/null
+	sleep 3
+	dhcpcd --waitip -t 7 2>/dev/null 1>/dev/null
+}
+
+warn() {
+	dialog --yesno "Changing MAC address will cause network distrutptions to this device.\nMake\
+	sure any file transfers or SSH sessions are complete before proceeding.\n\nDo you want to continue?" 10 52
+}
+
+auto() {
+	warn
+	if [ $? -ne 0 ]; then
+		return
+	fi
+
+	local macs=$(scan)
+	local mac=$(gen "$macs")
+
+	update_mac "$mac"
+
+	local check="$(cat /sys/class/net/$iface/address)"
+
+	if [ "$check" == "$mac" ]; then
+		update_uboot "$check"
+
+		local msg="The MAC address for $iface is now '$check'"
+		local len=$(( ${#msg} + 4 ))
+		len=$(( len + (len % 2) ))
+
+		dialog --msgbox "$msg" 5 "$len" 2>&1 >/dev/tty
+	else
+		dialog --msgbox "There was an error setting the MAC address" 5 52 2>&1 >/dev/tty
+	fi
+}
+
+main_menu() {
+	if [ ! -d "/sys/class/net/$iface" ]; then
+		dialog --msgbox "No interface named '$iface'.\nTry specifying an interface." 6 52  2>&1 >/dev/tty
+		exit 1
+	fi
+
+	if [ -n "$SSH_TTY" ]; then
+		dialog --msgbox "You appear to be running in a SSH session.\
+		Changing MAC address will cause network distruptions to this device.\
+		\n\nPlease run again locally on the device." 9 52  2>&1 >/dev/tty
+		exit 1
+	fi
+
+	while true; do
+		local addr=$(cat /sys/class/net/$iface/address)
+		choice=$(dialog --menu "The current MAC address for $iface is $addr\nWhat do you want to do?" 11 34 3 \
+			1 "Automatic Configuration" \
+			2 "Manual Configuration" \
+			2>&1 >/dev/tty)
+
+		dialog --clear
+
+		case $choice in
+			1)
+				auto
+			;;
+			2)
+				manual
+			;;
+			*)
+				exit 0
+			;;
+		esac
+	done
+}
+
+main_menu
